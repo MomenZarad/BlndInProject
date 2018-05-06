4d96a97d6d9f6e2662409183d7ef2e8a6b3c8e34
Zyad Galal <zyad_mg@yahooo.com>
2018-05-06 13:44:04 +0200

    check availability

--- blndIn/AppDelegate.swift
+++ blndIn/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
+    //this is final version
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
